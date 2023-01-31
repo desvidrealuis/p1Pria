ef2b1af2e5668b74fe51c79d34cf7f27ad96b4cd
Luis <3lr3n4ci0@gmail.com>
2023-01-31 21:08:20 +0100

    implimentación metodo GET

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(ObtenerPreguntas("https://opentdb.com/api.php?amount=50&category=25&difficulty=medium&type=multiple"));
+        StartCoroutine(ObtenerPreguntas("https://opentdb.com/api.php?amount=10&category=21&type=boolean"));
     }
 
     // Update is called once per frame
@@ -16,10 +17,31 @@ public class GameManager : MonoBehaviour
         
     }
 
-private IEnumerator ObtenerPreguntas(string pagWeb)
+    private IEnumerator ObtenerPreguntas(string pagWeb)
     {
-        yield return new WaitForSeconds(3);
-        Debug.Log("Función corrutinta prueba");
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(pagWeb))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = pagWeb.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+        
     }
 
 }
