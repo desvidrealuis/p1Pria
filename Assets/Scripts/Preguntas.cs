743919d456c2abb928fcb8e43c24e608b4cb5e1d
Luis <3lr3n4ci0@gmail.com>
2023-02-07 18:42:49 +0100

    desarrollo de clases

--- Assets/Scripts/Preguntas.cs
+++ Assets/Scripts/Preguntas.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Preguntas : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Preguntas {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string response_code;
+    public Pregunta[] results;
+
 }
